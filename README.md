a8e7322e7dbff6cf571f08436086889b229a7888
Gamerbful <polyor56@gmail.com>
2022-11-05 14:51:29 +0100

    final v2.0

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # feedCollector
 RSS-Intelligence project
+
+# elasticSearchTest
+
+to launch -> python .\elasticSearchTest.py [param]
+those parameters are
+- GEN to index our JSON rss file
+- COUNT to count how much indexed documents we have
+- SEARCH to test a query ( by default get all doc under an index)
--- elasticSearchTest.py
+++ elasticSearchTest.py
@@ -1,96 +1,48 @@
 from elasticsearch import Elasticsearch 
-import requests
-import pandas as pd
+import readJSON as rp
+import datetime
+from elasticsearch import helpers
 import sys
+es_client = Elasticsearch("http://localhost:9200")
 
+def generate_docs():
+    reader = rp.getRss()
 
+    for data in reader:
+        doc = {
+            "_index": "rss",
+            "_id": data["id"],
+            "_source": {
+                "id": data["id"],
+                "title": data["title"],
+                "description": data["description"],
+                "pubDate": data["pubDate"],
+                "link": data["link"],
+                "language": data["language"],
+                "Catégorie_du_flux": None,
+                "Catégorie_prédite": None,
+                "date_de_collecte": datetime.datetime.now(),
+                "data": data["data"] }
+            }
+        yield doc
 
+def clearServer():
+    es_client.delete_by_query(index="rss", query={"match_all": {}})
 
-es = Elasticsearch([{'host': 'localhost', 'port': 9200, "scheme": "http"}])
-
-
-
-def getESVersion(es):
-    #es = Elasticsearch()
-    print(es.info())
-
-
-def testServer():
-    response = requests.get('http://localhost:9200')
-    if response.status_code != 200:
-        print('ElasticSearch server not accessible !')
-    else: 
-        print('so far so good')
-
-
-def indexData(idx, data):
-    response = requests.get('http://localhost:9200')
-    if response.status_code != 200:
-        print('ElasticSearch server not accessible !')
-    else:
-        es.index(index='rss', id=idx, document = data)
-        print(idx, "indexed")
-
-
-
-def searchBdy(bdy, index_name):
-    try:
-        hits = es.search(index = index_name, query = bdy)
-        H = hits['hits']['hits']
-        print("# hits:", len(H))
-        for h in H:
-            print('>>', h['_score'], ' ', h['_source']['id'], h['_source']['numb_etud'])
-    except:
-        print('error:', sys.exc_info()[0])
-        hits=[]
-    return hits
-
-
-
-
-
+def getDocuments(index, query):
+    return es_client.search(index=index, query=query, size=10000)
 
 
 if __name__=='__main__':
-        
-    # reading
-    import readJSON as rj
-    rss = rj.getRss()
-    # testing
-    getESVersion(es)
-    testServer()
-    print(rss[0][1][1])
-    #indexing
-    #for i in range(len(rss)):
-    #    indexData(rss[i]['id'], rss[i])
-
-    # searching
-    #print('match all')
-    #bdy_match_all = {'match_all': {}}
-    #searchBdy(bdy_match_all)
-
-
-    # searching with a must
-    print('\n \n \nmust (and)')
-    bdy_must = {
-        "bool": {
-            'must': [
-                {'match': {'language': 'en'}}
-                #{'match': {'title':'Tucci samples sausage in crème brûlée'}}
-                #{'match': {'id': '60d0c7096c454ae0cd6d28ae8bb12f1119eba4b69a7bc4c0d24a8cabfd51f998'}}
-            ]
-        }
-    }
-
-    searchBdy(bdy_must)
-
-    #'Tucci samples sausage in crème brûlée'
-    """
-    for i in range(len(rss)):
-        #print(key)
-        print(rss[i]['title'])
-    """
-
-# id title description pubDate link language data
-# id : '60d0c7096c454ae0cd6d28ae8bb12f1119eba4b69a7bc4c0d24a8cabfd51f998'
-
+    if len(sys.argv) != 2:
+        print ( "See the README.MD to know how to launch")
+    else:
+        arg = sys.argv[1]
+        if arg == "GEN":
+            helpers.bulk(es_client, generate_docs())
+        if arg == "COUNT":
+            result = es_client.count(index="rss")
+            print(result.body['count'])
+        if arg == "SEARCH":
+            res = getDocuments("rss",{"match_all": {} } )
+            print ("query hits:", len(res["hits"]["hits"]))
